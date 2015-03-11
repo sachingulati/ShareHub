39dd61415cde13186d87f10d0a5231cea5d008fc
sachin <saching@intelligrape.com>
2015-03-11 12:50:14 +0530

    added properties in qa in datasource.groovy

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -46,6 +46,26 @@ environments {
             username = "sachin-gulati"
             password = "igdefault"
             url = "jdbc:mysql://127.0.0.1:3306/sachin-gulati_QA"
+            properties {
+                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
             //logSql = true
 
         }
